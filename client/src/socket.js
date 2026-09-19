ed4a419f4273876b25561eb3ae7da0cfc7fd2a93
Hiccuppp <37626942+Hiccuppp@users.noreply.github.com>
2026-09-19 23:03:29 +0800

    Add client game socket actions

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,25 @@
 import {io} from 'socket.io-client';
 
-export const socket=io();
+export const socket=io({
+  transports:['websocket','polling']
+});
 
 export function join(role,key){
- socket.emit('join',{role,key});
+  socket.emit('join',{role,key});
+}
+
+export function callLandlord(value){
+  socket.emit('call',Boolean(value));
+}
+
+export function robLandlord(value){
+  socket.emit('rob',Boolean(value));
+}
+
+export function playCards(cards){
+  socket.emit('play',cards);
+}
+
+export function passTurn(){
+  socket.emit('pass');
 }
