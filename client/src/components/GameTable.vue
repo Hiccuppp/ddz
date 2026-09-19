e47c829b0511bfa700070efbee53f5975dfd594e
Hiccuppp <37626942+Hiccuppp@users.noreply.github.com>
2026-09-19 22:56:42 +0800

    Add game table component

---
+++ client/src/components/GameTable.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="table">
+ <div class="opponent">对手 🂠 {{opponent}}</div>
+ <div class="center">{{phaseText}}</div>
+ <div class="hand">我的手牌 {{myCount}} 张</div>
+ <div class="buttons">
+  <button @click="$emit('call',true)">叫地主</button>
+  <button @click="$emit('call',false)">不叫</button>
+ </div>
+</div>
+</template>
+<script setup>
+defineProps({opponent:Number,myCount:Number,phaseText:String})
+defineEmits(['call'])
+</script>
